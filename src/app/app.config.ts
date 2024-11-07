a607a95b386f14f5f94625c18279e69a3e19550e
fouad-salhya <fouad.salhya@uit.ac.ma>
2024-11-07 18:18:55 +0100

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+       provideZoneChangeDetection({ eventCoalescing: true }), 
+       provideHttpClient(),
+       provideRouter(routes), 
+       provideStore(), 
+       provideEffects()
+      ]
 };
